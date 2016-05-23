7ffe13b907c9e6166914432d420463e87e7a170c
Jannis <jvajen@gmail.com>
2016-05-23 14:59:59 +0200

    Fix translation errors.

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,9 +1,3 @@
-from .production import *
+from .base import *
 
-DEBUG = True
-
-DEFAULT_FROM_EMAIL = "bestellungen@fahrwerk-berlin.de"
-EMAIL_HOST = "smtp.strato.de"
-EMAIL_HOST_USER = DEFAULT_FROM_EMAIL
-EMAIL_HOST_PASSWORD = "#e1nbEin!"
-EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
