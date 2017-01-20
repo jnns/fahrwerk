91103c7b5dd1902a4da07bf26fc0a28f9d1fbce4
jnns <jvajen@gmail.com>
2017-01-20 15:54:42 +0100

    Revert removal of EMAIL setting.

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,3 +14,12 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "bestellen.fahrwerk-berlin.de"]
 USE_X_FORWARDED_HOST = True
+
+# See https://wiki.uberspace.de/mail for configuration options
+EMAIL_HOST = os.environ["DJANGO_EMAIL_HOST"]
+EMAIL_PORT = os.environ["DJANGO_EMAIL_PORT"]
+EMAIL_HOST_USER = os.environ["DJANGO_EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = os.environ["DJANGO_EMAIL_HOST_PASSWORD"]
+
+DEFAULT_FROM_EMAIL = os.environ["DJANGO_DEFAULT_FROM_EMAIL"]
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
